202972f2cbb1a8c98300cfe06cb57fd99b262f42
taco650 <taco650@gmail.com>
2019-07-11 18:26:28 -0700

    Changed timing

--- FluxBot-1.0/lib/constants.py
+++ FluxBot-1.0/lib/constants.py
@@ -5,13 +5,13 @@ ACTUATION_TIME = 13
 #Position of the actuator when it is fully retracted
 ACTUATION_RETRACTION_POSITION = 0
 #Position of the actuator when it is fully extended
-ACTUATION_EXTENSION_POSITION = 1
+ACTUATION_EXTENSION_POSITION = 0.9
 #Name that is put on the CSV
 DEVICE_NAME = "Test"
 #ms unresponsive until device resets
 WATCHDOG_TIMEOUT = 7*1000
 #Time in seconds for each measurement cycle (Start every 58 mins)
-CYCLE_PERIOD = 1*60*60
+CYCLE_PERIOD = 1*60*55
 #Time for cycle (seconds for measurement process with cloesd lid)(2 min)
 CYCLE_LENGTH = 1*60
 #Measurement freq in closed box (Hz)
@@ -19,7 +19,7 @@ MEASUREMENT_CLOSED_BOX_FRQ = 1
 #Measurement freq w/ open box
 MEASUREMENT_OPEN_BOX_FRQ = 21
 #Time between open bursts in secs
-BURST_PERIOD = 20*60
+BURST_PERIOD = 18*60
 #Num of Points in Open Box Burst
 OPEN_BURST_POINTS = 3
 #Delay (ms) Between open box burst points
@@ -30,4 +30,4 @@ CLOSED_BURST_POINTS = 60*5
 #twofreshmeasurementspersecond
 CLOSED_BURST_DELAY = 1000
 
-BURST_DELAY_AFTER_OPEN = 5 * 60
+BURST_DELAY_AFTER_OPEN = 18 * 60
