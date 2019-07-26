3dd6b2d3cb1e94ac80c9b6f3344684bff985a479
Vincent Benenati <taco650@gmail.com>
2019-07-26 15:26:23 -0700

    Added timestamp to date script

---
+++ timestampToDate.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from os import walk
+from shutil import copyfile
+import csv
+import os.path
+
+
+#copyfile("./Test.csv", "./TestDuplicate.csv")
+f = []
+for (dirpath, dirnames, filenames) in walk("./"):
+    f.extend(filenames)
+    break
+
+dataFiles = []
+for file in f:
+    if ".csv" in file and '_Fluxbot_Data_' in file and not "_Dated" in file and not os.path.exists(file[:-4] + "_Dated.csv"):
+        dataFiles.append(file)
+
+print(dataFiles)
+
+for file in dataFiles:
+    with open(file,'r') as csvinput:
+        with open(file[:-4] + "_Dated.csv", 'w') as csvoutput:
+            writer = csv.writer(csvoutput, lineterminator='\n')
+            reader = csv.reader(csvinput)
+
+            all = []
+            row = next(reader)
+            row.append('Year')
+            row.append('Month')
+            row.append('Day')
+            row.append('Hour')
+            row.append('Minute')
+            row.append('Second')
+            all.append(row)
+
+            for row in reader:
+                time = datetime.utcfromtimestamp(int(row[0]))
+                row.append(time.strftime("%Y"))
+                row.append(time.strftime("%m"))
+                row.append(time.strftime("%d"))
+                row.append(time.strftime("%H"))
+                row.append(time.strftime("%M"))
+                row.append(time.strftime("%S"))
+                all.append(row)
+
+            writer.writerows(all)
